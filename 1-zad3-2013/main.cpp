564bded17f773d001da0d35db036971cc67d2140
hawkerpl <m.m.hawker@gmail.com>
2014-02-02 12:20:49 +0100

    zmiana nazwy

---
+++ 1-zad3-2013/main.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <typename T=int> struct box{
+        T val;
+        box(){
+
+        }
+        box(T a){
+                        val=a;
+                }
+                operator T() const {
+                        return val;
+                }
+};
+
+template <typename T, int S=2> struct array{
+        mutable vector<T> v;
+        array( const array<T,2> &in){
+                v=in.v;
+        }
+        array(){
+                v = vector<T>(S);
+        }
+        T &operator[](int i) const {
+                return v[i];
+        }
+        operator const array<T>(){
+
+        }
+};
+
+
+
+int main(){
+        array<box<>,2> a;
+        double i = a[0]=a[1]=1;
+        const array<box<int> > b= a;
+        b[0] = b[1] = 2;
+}
