8fcac79d017d4c0575993537685461bb9f4db287
hawkerpl <m.m.hawker@gmail.com>
2014-02-02 12:21:36 +0100

    3 2 term

--- 2-zad3-2013/main.cpp
+++ 2-zad3-2013/main.cpp
@@ -1,41 +1,57 @@
-#include <vector>
+/*
+ * main.cpp
+ *
+ *  Created on: 2 lut 2014
+ *      Author: hawker
+ */
+
+
+
+
 #include <iostream>
+
 using namespace std;
 
-template <typename T=int> struct box{
-        T val;
-        box(){
+template <typename T=double>
+class box
+{
+        private:
+                T value;
 
-        }
-        box(T a){
-                        val=a;
-                }
-                operator T() const {
-                        return val;
+        public:
+                box(T v): value(v) {}
+
+                void print()
+                {
+                        cout << (double)value << "\n";
                 }
 };
 
-template <typename T, int S=2> struct array{
-        mutable vector<T> v;
-        array( const array<T,2> &in){
-                v=in.v;
-        }
-        array(){
-                v = vector<T>(S);
-        }
-        T &operator[](int i) const {
-                return v[i];
-        }
-        operator const array<T>(){
+template <>
+class box<char>
+{
+        private:
+                char value;
 
-        }
+        public:
+                explicit box(char v): value(v) {}
+
+                void print()
+                {
+                        cout << (double)value << "\n";
+                }
 };
 
 
+box<> a = 2.2;
+box<int> b = 3;
+box<char> c = box<char>('A');
+//box<char> d = 'A';
+//odkomentowanie tej linijki powoduje blad kompilacji
 
-int main(){
-        array<box<>,2> a;
-        double i = a[0]=a[1]=1;
-        const array<box<int> > b= a;
-        b[0] = b[1] = 2;
+int main()
+{
+        a.print();              // Wypisuje obiekt T: 2.2
+        b.print();              // Wypisuje obiekt T: 3
+        c.print();              // Wypisuje obiekt char: 65
 }
